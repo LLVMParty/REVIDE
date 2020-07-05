6d957910e78073977b3591eef9883256bce0d249
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2020-07-05 04:14:58 +0200

    Minor improvements for demo

--- REVIDE/BitcodeHighlighter.cpp
+++ REVIDE/BitcodeHighlighter.cpp
@@ -9,20 +9,31 @@ BitcodeHighlighter::BitcodeHighlighter(QTextDocument *parent)
     keywordFormat.setFontWeight(QFont::Bold);
     const QString keywords [] =
     {
-        "source_filename",
-        "target",
-        "datalayout",
-        "triple",
-        "constant",
-        "align",
-        "appending",
-        "global",
-        "bitcast",
-        "to",
-        "section",
-        "define",
-        "linkonce_odr",
-        // TODO
+        // https://github.com/compiler-explorer/compiler-explorer/blob/6eab83562af4c81269222fc0e7b12092884e0912/static/modes/llvm-ir-mode.js#L56
+        "acq_rel", "acquire", "addrspace", "alias", "align", "alignstack", "alwaysinline",
+        "appending", "argmemonly", "arm_aapcscc", "arm_aapcs_vfpcc", "arm_apcscc", "asm",
+        "atomic", "available_externally", "blockaddress", "builtin", "byval", "c", "catch",
+        "caller", "cc", "ccc", "cleanup", "coldcc", "comdat", "common", "constant",
+        "datalayout", "declare", "default", "define", "deplibs", "dereferenceable",
+        "distinct", "dllexport", "dllimport", "dso_local", "dso_preemptable", "except",
+        "external", "externally_initialized", "extern_weak", "fastcc", "filter", "from",
+        "gc", "global", "hhvmcc", "hhvm_ccc", "hidden", "initialexec", "inlinehint",
+        "inreg", "inteldialect", "intel_ocl_bicc", "internal", "linkonce", "linkonce_odr",
+        "localdynamic", "localexec", "local_unnamed_addr", "minsize", "module",
+        "monotonic", "msp430_intrcc", "musttail", "naked", "nest", "noalias", "nobuiltin",
+        "nocapture", "noimplicitfloat", "noinline", "nonlazybind", "nonnull", "norecurse",
+        "noredzone", "noreturn", "nounwind", "optnone", "optsize", "personality",
+        "private", "protected", "ptx_device", "ptx_kernel", "readnone", "readonly",
+        "release", "returned", "returns_twice", "sanitize_address", "sanitize_memory",
+        "sanitize_thread", "section", "seq_cst", "sideeffect", "signext", "syncscope",
+        "source_filename", "speculatable", "spir_func", "spir_kernel", "sret", "ssp",
+        "sspreq", "sspstrong", "strictfp", "swiftcc", "tail", "target", "thread_local",
+        "to", "triple", "unnamed_addr", "unordered", "uselistorder", "uselistorder_bb",
+        "uwtable", "volatile", "weak", "weak_odr", "within", "writeonly", "x86_64_sysvcc",
+        "win64cc", "x86_fastcallcc", "x86_stdcallcc", "x86_thiscallcc", "zeroext",
+
+        // Additional keywords
+        "source_filename", "nofree",
     };
     for (const QString &pattern : keywords) {
         rule.pattern = QRegularExpression(QString("\\b%1\\b").arg(pattern));
