a275877ac444865c849f600045141218847ea2cd
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2024-06-27 17:22:38 +0200

    Update FindLLVM-Wrapper.cmake

--- CMake/FindLLVM-Wrapper.cmake
+++ CMake/FindLLVM-Wrapper.cmake
@@ -25,21 +25,39 @@ endif()
 
 # Find LLVM
 find_package(LLVM ${FIND_ARGS})
+unset(FIND_ARGS)
 
-message(STATUS "Found LLVM ${LLVM_PACKAGE_VERSION}")
-message(STATUS "Using LLVMConfig.cmake in: ${LLVM_DIR}")
+if(NOT LLVM-Wrapper_FIND_QUIETLY)
+    message(STATUS "Found LLVM ${LLVM_PACKAGE_VERSION}")
+    message(STATUS "Using LLVMConfig.cmake in: ${LLVM_DIR}")
+endif()
 
 # Split the definitions properly (https://weliveindetail.github.io/blog/post/2017/07/17/notes-setup.html)
 separate_arguments(LLVM_DEFINITIONS)
 
+# https://github.com/JonathanSalwan/Triton/issues/1082#issuecomment-1030826696
+if(LLVM_LINK_LLVM_DYLIB)
+    set(LLVM-Wrapper_LIBS LLVM)
+elseif(LLVM-Wrapper_FIND_COMPONENTS)
+    if(NOT LLVM-Wrapper_FIND_QUIETLY)
+        message(STATUS "LLVM components: ${LLVM-Wrapper_FIND_COMPONENTS}")
+    endif()
+    llvm_map_components_to_libnames(LLVM-Wrapper_LIBS ${LLVM-Wrapper_FIND_COMPONENTS})
+else()
+    set(LLVM-Wrapper_LIBS ${LLVM_AVAILABLE_LIBS})
+endif()
+
 # Some diagnostics (https://stackoverflow.com/a/17666004/1806760)
-message(STATUS "LLVM libraries: ${LLVM_AVAILABLE_LIBS}")
-message(STATUS "LLVM includes: ${LLVM_INCLUDE_DIRS}")
-message(STATUS "LLVM definitions: ${LLVM_DEFINITIONS}")
-message(STATUS "LLVM tools: ${LLVM_TOOLS_BINARY_DIR}")
+if(NOT LLVM-Wrapper_FIND_QUIETLY)
+    message(STATUS "LLVM libraries: ${LLVM-Wrapper_LIBS}")
+    message(STATUS "LLVM includes: ${LLVM_INCLUDE_DIRS}")
+    message(STATUS "LLVM definitions: ${LLVM_DEFINITIONS}")
+    message(STATUS "LLVM tools: ${LLVM_TOOLS_BINARY_DIR}")
+endif()
 
 add_library(LLVM-Wrapper INTERFACE IMPORTED)
 target_include_directories(LLVM-Wrapper SYSTEM INTERFACE ${LLVM_INCLUDE_DIRS})
+target_link_libraries(LLVM-Wrapper INTERFACE ${LLVM-Wrapper_LIBS})
 target_compile_definitions(LLVM-Wrapper INTERFACE ${LLVM_DEFINITIONS})
 
 # Set the appropriate minimum C++ standard
@@ -56,29 +74,21 @@ endif()
 
 if(WIN32)
     target_compile_definitions(LLVM-Wrapper INTERFACE NOMINMAX)
-endif()
 
-# https://github.com/JonathanSalwan/Triton/issues/1082#issuecomment-1030826696
-if(LLVM_LINK_LLVM_DYLIB)
-    target_link_libraries(LLVM-Wrapper INTERFACE LLVM)
-else()
-    target_link_libraries(LLVM-Wrapper INTERFACE ${LLVM_AVAILABLE_LIBS})
-endif()
-
-# In LLVM 10 (and potentially below) there is a full path to diaguids.lib embedded in the installation
-if(WIN32 AND TARGET LLVMDebugInfoPDB)
-    get_target_property(LLVMDebugInfoPDB_LIBS LLVMDebugInfoPDB INTERFACE_LINK_LIBRARIES)
-    foreach(LLVMDebugInfoPDB_LIB ${LLVMDebugInfoPDB_LIBS})
-        if(LLVMDebugInfoPDB_LIB MATCHES "diaguids.lib")
-            list(REMOVE_ITEM LLVMDebugInfoPDB_LIBS "${LLVMDebugInfoPDB_LIB}")
-            list(APPEND LLVMDebugInfoPDB_LIBS "diaguids.lib")
-            break()
-        endif()
-    endforeach()
-    set_target_properties(LLVMDebugInfoPDB PROPERTIES
-        INTERFACE_LINK_LIBRARIES "${LLVMDebugInfoPDB_LIBS}"
-    )
-    unset(LLVMDebugInfoPDB_LIBS)
+    # This target has an unnecessary diaguids.lib embedded in the installation
+    if(TARGET LLVMDebugInfoPDB)
+        get_target_property(LLVMDebugInfoPDB_LIBS LLVMDebugInfoPDB INTERFACE_LINK_LIBRARIES)
+        foreach(LLVMDebugInfoPDB_LIB ${LLVMDebugInfoPDB_LIBS})
+            if(LLVMDebugInfoPDB_LIB MATCHES "diaguids.lib")
+                list(REMOVE_ITEM LLVMDebugInfoPDB_LIBS "${LLVMDebugInfoPDB_LIB}")
+                break()
+            endif()
+        endforeach()
+        set_target_properties(LLVMDebugInfoPDB PROPERTIES
+            INTERFACE_LINK_LIBRARIES "${LLVMDebugInfoPDB_LIBS}"
+        )
+        unset(LLVMDebugInfoPDB_LIBS)
+    endif()
 endif()
 
 set(CMAKE_FOLDER "${CMAKE_FOLDER_LLVM}")
--- REVIDE/cutter/README.md
+++ REVIDE/cutter/README.md
@@ -1,3 +1,3 @@
 # Cutter
 
-The code here is based on https://github.com/rizinorg/cutter commit hash `8f0f176f086337affb2c57013a8b96cc0f11f4ad`.
+The code here is based on https://github.com/rizinorg/cutter commit hash `8ad700a6dc1a4a628f23ebefa2138c5f8df0182d`.
